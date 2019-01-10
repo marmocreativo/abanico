23f32ac914b92aa904feb1bc4ab470c2660d074b
Andres Ruelas <triad.diseno@gmail.com>
2019-01-09 18:31:59 -0600

    Vista de Servicio
    html de servicio en test.php y nuevos estilos en desktop

--- aplicacion/views/desktop/tienda/test.php
+++ aplicacion/views/desktop/tienda/test.php
@@ -1,3 +1,125 @@
 <div class="contenido_principal">
-
+  <div class="main">
+      <div class="serv-profile">
+        <div class="jumbotron mb-2">
+        </div>
+        <div class="container">
+          <div class="row mb-5">
+            <div class="col-3">
+              <img src="http://lorempixel.com/180/180/people/9/" class="img-thumbnail image-profile float-left" alt="Profile image example">
+            </div>
+            <div class="col-9">
+                <nav aria-label="breadcrumb">
+                  <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="<?php echo base_url(); ?>">Inicio</a></li>
+                    <li class="breadcrumb-item active <?php echo 'text'.$primary; ?>" aria-current="page">Mis servicios</li>
+                  </ol>
+                </nav>
+                <h2>Nombre del prestador de servicios</h2>
+                <h6>Nombre del prestador de servicios</h6>
+            </div>
+          </div>
+          <div class="row mb-5">
+            <div class="col-12">
+              <div class="card card-desc-servicio">
+                <div class="card-header">
+                  <h5 class="card-title">Descripción del servicio</h5>
+                </div>
+                <div class="card-body fila-gris">
+                  <div class="row">
+                    <p class="card-text col-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel sem at leo sagittis lacinia. Nulla in tincidunt risus. Quisque et nisl dolor. Integer urna ante, cursus at magna sit amet, maximus ultricies eros. Sed tincidunt laoreet metus eget pellentesque. Sed at velit lectus. Cras gravida turpis vel congue varius.</p>
+                    <p class="card-text col-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel sem at leo sagittis lacinia. Nulla in tincidunt risus. Quisque et nisl dolor. Integer urna ante, cursus at magna sit amet, maximus ultricies eros. Sed tincidunt laoreet metus eget pellentesque. Sed at velit lectus. Cras gravida turpis vel congue varius.</p>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row mb-5">
+            <div class="col-8">
+              <div class="card-group">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">Zona de Trabajo</h5>
+                    <p class="card-text">Pais:</p>
+                    <p class="card-text">Estado:</p>
+                  </div>
+                  <div class="card-footer">
+                    <small class="text-muted">Horario de trabajo: 10:00 a 18:00</small>
+                  </div>
+                </div>
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">Colonias con servicio</h5>
+                    <ul class="list-group list-group-flush">
+                      <li class="list-group-item">Colonia 1</li>
+                      <li class="list-group-item">Colonia 2</li>
+                      <li class="list-group-item">Colonia 3</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-4">
+              <button type="button" class="btn btn-primary-18 btn-lg btn-block btn-contacto">Contactar ahora</button>
+            </div>
+          </div>
+          <div class="row mb-5">
+            <div class="container-fluid fila-gris py-4">
+              <div class="col-12">
+                <div class="card opiniones-serv">
+                  <div class="card-body">
+                    <h5 class="card-title">Calificación promedio del servicio</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Opiniones</h6>
+                    <div class="list-group">
+                      <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                        <div class="d-flex w-100 justify-content-between">
+                          <ul class="rating">
+                            <li class="fa fa-star <?php echo 'text'.$primary; ?>"></li>
+                            <li class="fa fa-star <?php echo 'text'.$primary; ?>"></li>
+                            <li class="fa fa-star <?php echo 'text'.$primary; ?>"></li>
+                            <li class="fa fa-star <?php echo 'text'.$primary; ?>"></li>
+                            <li class="fa fa-star <?php echo 'text'.$primary; ?>"></li>
+                          </ul>
+                          <small>Hace 3 días</small>
+                        </div>
+                        <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                        <small>Usuario 3</small>
+                      </a>
+                      <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                        <div class="d-flex w-100 justify-content-between">
+                          <ul class="rating">
+                            <li class="fa fa-star <?php echo 'text'.$primary; ?>"></li>
+                            <li class="fa fa-star <?php echo 'text'.$primary; ?>"></li>
+                            <li class="fa fa-star <?php echo 'text'.$primary; ?>"></li>
+                            <li class="fa fa-star <?php echo 'text'.$primary; ?>"></li>
+                            <li class="fa fa-star <?php echo 'text'.$primary; ?>"></li>
+                          </ul>
+                          <small class="text-muted">Hace 3 días</small>
+                        </div>
+                        <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                        <small class="text-muted">Usuario 2</small>
+                      </a>
+                      <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                        <div class="d-flex w-100 justify-content-between">
+                          <ul class="rating">
+                            <li class="fa fa-star <?php echo 'text'.$primary; ?>"></li>
+                            <li class="fa fa-star <?php echo 'text'.$primary; ?>"></li>
+                            <li class="fa fa-star <?php echo 'text'.$primary; ?>"></li>
+                            <li class="fa fa-star <?php echo 'text'.$primary; ?>"></li>
+                            <li class="fa fa-star <?php echo 'text'.$primary; ?>"></li>
+                          </ul>
+                          <small class="text-muted">Hace 3 días</small>
+                        </div>
+                        <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                        <small class="text-muted">Usuario 1</small>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div> <!-- /container -->
 </div>
