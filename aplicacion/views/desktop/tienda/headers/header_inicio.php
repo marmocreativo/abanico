4f1b881558002bc0fd0234b226a9e7adb3f86643
Andres Ruelas <triad.diseno@gmail.com>
2019-01-14 16:16:04 -0600

    Update header_inicio.php
    Cambios en header

--- aplicacion/views/desktop/tienda/headers/header_inicio.php
+++ aplicacion/views/desktop/tienda/headers/header_inicio.php
@@ -37,15 +37,15 @@
       <nav class="navbar navbar-expand">
         <div class="d-flex justify-content-arround align-items-center">
           <a class="navbar-brand mr-1" href="<?php echo base_url(); ?>"><img src="<?php echo base_url(); ?>assets/global/img/logo.png" alt=""></a>
-          <a class="btn <?php echo 'btn-outline'.$primary; ?> mr-3" data-toggle="collapse" href="#menu-categorias" role="button" aria-expanded="false" aria-controls="menu-categorias">CATEGORÍAS <span class="fa fa-list"></span> </a>
+          <a class="btn <?php echo 'btn-link'.$primary; ?> mr-3" data-toggle="collapse" href="#menu-categorias" role="button" aria-expanded="false" aria-controls="menu-categorias"><i class="fas fa-angle-down mr-2"></i>CATEGORÍAS</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
         </div>
 
-        <div class="collapse navbar-collapse d-flex justify-content-between">
-          <form class="w-75" action="<?php echo base_url('categoria/busqueda') ?>" method="get">
-            <div class="input-group">
+        <div class="collapse navbar-collapse">
+          <form class="form-inline my-2 my-lg-0 mr-auto w-75" action="<?php echo base_url('categoria/busqueda') ?>" method="get">
+            <div class="input-group w-100">
               <input class="form-control w-50" type="search" name="Busqueda" placeholder="Busca lo Mejor" aria-label="Search">
               <select class="form-control" aria-label="Example select with button addon">
                 <option value="todo">En Todo</option>
@@ -57,10 +57,14 @@
               </div>
             </div>
           </form>
-          <div class="w-25 d-flex justify-content-end">
-            <a class="btn <?php echo 'btn-outline'.$primary; ?>" href="<?php echo base_url('usuario/favoritos'); ?>"> <span class="fa fa-heart"></span> Favoritos</a>
-            <button class="btn <?php echo 'btn-outline'.$primary; ?>  ml-3" data-toggle="modal" data-target="#ModalCarrito"> <span class="fa fa-shopping-cart"></span> Carrito</button>
-          </div>
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link btn-sm" href="<?php echo base_url('usuario/favoritos'); ?>"><i class="far fa-heart text-primary-6"></i> Favoritos</a>
+            </li>
+            <li class="nav-item">
+              <button type="button" class="btn btn-sm <?php echo 'btn-link'.$primary; ?> text-primary" data-toggle="modal" data-target="#ModalCarrito" style="background:transparent;"> <span class="fa fa-shopping-cart text-primary-1"></span> Carrito</button>
+            </li>
+          </ul>
         </div>
       </nav>
     </div>
