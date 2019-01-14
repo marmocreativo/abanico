9d63eed5b68b1d193e18792389647e13a30fed27
marmocreativo <marmocreativo@gmail.com>
2019-01-14 17:06:39 -0600

    Activadas vistas mobile Index

--- aplicacion/controllers/Tienda_Inicio.php
+++ aplicacion/controllers/Tienda_Inicio.php
@@ -14,7 +14,7 @@ class Tienda_Inicio extends CI_Controller {
 		$this->data['primary'] = "-primary";
 
 		if($this->agent->is_mobile()){
-			$this->data['dispositivo']  = "desktop";
+			$this->data['dispositivo']  = "mobile";
 		}else{
 			$this->data['dispositivo']  = "desktop";
 		}
