189e66cbb5f775d6d84b209986b942ef04852c5c
Andres Ruelas <triad.diseno@gmail.com>
2019-01-16 15:39:05 -0600

    Vista de categorías
    Se agrega la vista de categorías con el css actualizado

---
+++ aplicacion/views/desktop/tienda/test_todasCategorias.php
@@ -0,0 +1,33 @@
+<div class="contenido_principal">
+  <div class="container">
+    <nav class="my-3" aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="<?php echo base_url(); ?>">Inicio</a></li>
+        <li class="breadcrumb-item active <?php echo 'text'.$primary; ?>" aria-current="page">Mis servicios</li>
+      </ol>
+    </nav>
+    <div class="row">
+      <div class="col-3">
+        <div class="card" id="menuTodasCategorias">
+          <div class="card-header">
+            Categorías
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item link-primary"><a href=""><i class="fas fa-laptop"></i> Categoría 1</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-9">
+        <div class="row">
+          <h5 class="col-12 text-primary pb-2" id="tituloCategoria"><i class="fas fa-laptop"></i> Tecnología, Computación y Gadgets</h5>
+          <div class="col-4">
+            <h6>Audio</h6>
+            <ul class="list list-unstyled">
+              <li><a href="http://localhost/abanico-master/categoria?slug=audifonos">Audifonos</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
