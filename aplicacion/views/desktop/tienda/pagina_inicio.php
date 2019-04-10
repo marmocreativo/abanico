32afd9fb4cd6c6f9dd89aa2688242eff59c5c4ae
Andrés Ruelas <pixelbiff@gmail.com>
2019-04-09 19:30:15 -0500

    Cambios a slider de ayuda

--- aplicacion/views/desktop/tienda/pagina_inicio.php
+++ aplicacion/views/desktop/tienda/pagina_inicio.php
@@ -377,12 +377,20 @@
   <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLongTitle"><i class="far fa-question-circle"></i> Ayuda</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <div class="modal-body">
+      <div class="modal-body p-0">
+        
+          <div class="linea-colores-delgada">
+            <div class="barra-color barra-azul"></div>
+            <div class="barra-color barra-rosa"></div>
+            <div class="barra-color barra-amarillo"></div>
+            <div class="barra-color barra-verde"></div>
+            <div class="barra-color barra-morado"></div>
+          </div>
         <!-- Slider Ayuda-->
         <div id="carouselAyuda" class="carousel slide" data-ride="carousel">
           <ol class="carousel-indicators">
