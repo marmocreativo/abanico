9949c33f171cdd4e4c7b0f5896bcdc92b45e3599
marmocreativo <marmocreativo@gmail.com>
2019-09-05 12:29:49 -0500

    analytics en usuarios

--- aplicacion/views/desktop/usuarios/headers/header.php
+++ aplicacion/views/desktop/usuarios/headers/header.php
@@ -16,6 +16,27 @@
     <link href="<?php echo base_url('assets/metronic/'); ?>vendors/general/summernote/dist/summernote.css" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/global/css/estilos_abanico_desktop.css">
     <title>Usuarios | <?php echo $op['titulo_sitio']; ?></title>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-143900653-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-143900653-1');
+    </script>
+
+    <!-- Hotjar Tracking Code for https://www.abanicoytu.com -->
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:1438316,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
   </head>
   <body>
     <!-- Header -->
