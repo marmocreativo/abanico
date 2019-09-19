177fc088734c510f74d6785cf00b4985b6731cda
marmocreativo <marmocreativo@gmail.com>
2019-09-19 11:45:38 -0500

    animación scroll

---
+++ aplicacion/logs/log-2019-09-19.php
@@ -0,0 +1,27 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2019-09-19 10:30:58 --> UTF-8 Support Enabled
+DEBUG - 2019-09-19 10:30:58 --> No URI present. Default controller set.
+DEBUG - 2019-09-19 10:30:58 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-09-19 10:30:59 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+ERROR - 2019-09-19 10:31:01 --> Severity: Notice --> Undefined variable: premio D:\xampp\htdocs\abanico-master\aplicacion\views\desktop\tienda\pagina_inicio.php 255
+ERROR - 2019-09-19 10:31:01 --> Severity: Notice --> Undefined variable: premio D:\xampp\htdocs\abanico-master\aplicacion\views\desktop\tienda\pagina_inicio.php 263
+DEBUG - 2019-09-19 10:31:02 --> Total execution time: 4.3711
+DEBUG - 2019-09-19 10:31:02 --> UTF-8 Support Enabled
+DEBUG - 2019-09-19 10:31:02 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2019-09-19 10:31:02 --> 404 Page Not Found: Assets/global
+DEBUG - 2019-09-19 10:31:03 --> UTF-8 Support Enabled
+DEBUG - 2019-09-19 10:31:03 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2019-09-19 10:31:03 --> 404 Page Not Found: Assets/global
+DEBUG - 2019-09-19 10:31:03 --> UTF-8 Support Enabled
+DEBUG - 2019-09-19 10:31:03 --> UTF-8 Support Enabled
+DEBUG - 2019-09-19 10:31:03 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-09-19 10:31:03 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-09-19 10:31:03 --> UTF-8 Support Enabled
+DEBUG - 2019-09-19 10:31:03 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-09-19 10:31:03 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-09-19 10:31:03 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-09-19 10:31:03 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-09-19 10:31:03 --> Total execution time: 0.1792
+DEBUG - 2019-09-19 10:31:03 --> Total execution time: 0.3866
+DEBUG - 2019-09-19 10:31:03 --> Total execution time: 0.4840
