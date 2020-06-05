fe36bd087207cc150cb995ffbddd2d7ebf398b61
marmocreativo <marmocreativo@gmail.com>
2020-06-05 18:42:19 -0500

    Create log-2020-06-05.php

---
+++ aplicacion/logs/log-2020-06-05.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2020-06-05 18:40:32 --> UTF-8 Support Enabled
+DEBUG - 2020-06-05 18:40:32 --> No URI present. Default controller set.
+DEBUG - 2020-06-05 18:40:33 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2020-06-05 18:40:37 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): No se puede establecer una conexi�n ya que el equipo de destino deneg� expresamente dicha conexi�n.
+ D:\xampp\htdocs\abanico-master\sistema\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2020-06-05 18:40:37 --> Unable to connect to the database
